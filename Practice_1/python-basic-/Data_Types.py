40a9abe395426fa63a3b18227b5bdfca81e44fd1
ismagulovdias62-glitch <ismagulovdias62@gmail.com>
2026-02-03 15:28:19 +0500

    Add files via upload

---
+++ Practice_1/python-basic-/Data_Types.py
@@ -0,0 +1,14 @@
+x = 5
+print(type(x))
+
+
+x = str("Hello World")	
+
+
+x = int(20)	
+
+
+x = float(20.5)	
+
+
+x = complex(1j)
---
+++ Practice_1/python-basic-/Strings.py
@@ -0,0 +1,22 @@
+a = "Hello"
+print(a)
+
+
+b = "Hello, World!"
+print(b[2:5])
+
+
+a = "Hello, World!"
+print(a.upper())
+
+
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+
+
+age = 36
+txt = f"My name is John, I am {age}"
+print(txt)
